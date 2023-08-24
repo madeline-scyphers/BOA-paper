6d116e282a1048f1168ca0f0a11f0ad66c0ff067
Madeline Scyphers <madelinescyphers@gmail.com>
2023-08-24 12:38:01 -0400

    MOO SAASBO updates

--- saasbo_moo/wrapper.py
+++ saasbo_moo/wrapper.py
@@ -19,13 +19,25 @@ class WrapperSaasboMoo(BaseWrapper):
     def set_trial_status(self, trial) -> None:
         trial.mark_completed()
 
-    def fetch_trial_data(self, parameters, **kwargs):
-        return {"a": f1(list(parameters.values())), "b": f2(list(parameters.values()))}
+    def fetch_trial_data(self, parameters, param_names, metric_name, **kwargs):
+
+        res = f([param for name, param in parameters.items() if name in param_names])
+        return {
+            f"obj{i}": float(res[i]) for i in range(n_obj)
+        }
+        # return {"a": f_a([param for name, param in parameters.items() if name in self.metric_params["a"]]),
+        #         "b": f_b([param for name, param in parameters.items() if name in self.metric_params["b"]])}
+        #
+        # return {"a": f1(list(parameters.values())), "b": f2(list(parameters.values()))}
 
 
-def f1(x) -> float:
+def f(x):
+    return problem(torch.tensor(x, **tkwargs).clamp(0.0, 1.0))
+
+
+def f_a(x) -> float:
     return float(problem(torch.tensor(x, **tkwargs).clamp(0.0, 1.0))[0])
 
 
-def f2(x) -> float:
+def f_b(x) -> float:
     return float(problem(torch.tensor(x, **tkwargs).clamp(0.0, 1.0))[1])
