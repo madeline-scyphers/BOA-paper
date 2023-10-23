ea23623fa42d03b23aaccfd7de43657a02ee23e8
Madeline Scyphers <madelinescyphers@gmail.com>
2023-10-23 13:36:38 -0400

    Reduce swat run_model to only comparing flow out

--- swat/run_model.R
+++ swat/run_model.R
@@ -21,8 +21,6 @@ for (name in names(data)) {
     par_comb[paste(name, " | change = absval")] = data[[name]]
 }
 
-print(par_comb)
-
 sim <- run_swatplus(
     project_path=project_path,
     start_date = start_date,
@@ -31,60 +29,25 @@ sim <- run_swatplus(
     run_path=trial_dir,
     keep_folder=TRUE,
     parameter = par_comb,
-    output = list(
-        flo_out = define_output(
-            file = 'reservoir_day',
-            # file = 'channel_sd_day',
+    output = define_output(
+            file = 'channel_sd_day',
             variable = 'flo_out',
-            unit = 1),
-        solp_out = define_output(
-            file = 'reservoir_day',
-            variable = 'solp_out',
-            unit = 1),
-        sedp_out = define_output(
-            file = 'reservoir_day',
-            variable = 'sedp_out',
-            unit = 1),
-        sed_out = define_output(
-            file = 'reservoir_day',
-            variable = 'sed_out',
             unit = 1)
-            )
         )
 
 obs<-read_csv(here("swat","obs_data.csv"))
 obs <- obs %>% filter(between(date, as.Date(start_date), as.Date(end_date)))
 
 flow_obs <- obs %>% filter(variable == "flow_cms") %>% within(rm(variable))
-solp_obs <- obs %>% filter(variable == "SRP_kg") %>% within(rm(variable))
-sed_obs <- obs %>% filter(variable == "SS_ton") %>% within(rm(variable))
-totp_obs <- obs %>% filter(variable == "TP_kg") %>% within(rm(variable)) %>% filter(date %in% solp_obs$date)
-
 flow_sim <- sim$simulation$flo_out %>% filter(date %in% flow_obs$date)
-solp_sim <- sim$simulation$solp_out %>% filter(date %in% totp_obs$date)
-sedp_sim <- sim$simulation$sedp_out %>% filter(date %in% totp_obs$date)
-sed_sim <- sim$simulation$sed_out %>% filter(date %in% sed_obs$date)
-totp_sim <- within(sedp_sim, rm(date)) + within(solp_sim, rm(date))
 
 if (!is.null(flow_sim)) {
     jsn <- toJSON(list(
         flo_out=list(
             y_pred=flow_sim$run_1,
             y_true=flow_obs$value
-        ),
-        totp=list(
-            y_pred=totp_sim$run_1,
-            y_true=totp_obs$value
-        ),
-        solp_out=list(
-            y_pred=solp_sim$run_1,
-            y_true=solp_obs$value
-        ),
-        sed_out=list(
-            y_pred=sed_sim$run_1,
-            y_true=sed_obs$value
         )
-    ), pretty = FALSE)
+    ), pretty = TRUE)
     write(jsn, file.path(trial_dir, "output.json"))
 } else {
     write(toJSON(list(trial_status=unbox("FAILED"))), file.path(trial_dir, "output.json"))
