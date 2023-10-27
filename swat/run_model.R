6dc3b2c8308062d2e3d8f8d8224b4df04b2353bd
Madeline Scyphers <madelinescyphers@gmail.com>
2023-10-27 11:49:15 -0400

    Working NRE run_model script

--- swat/run_model.R
+++ swat/run_model.R
@@ -7,6 +7,7 @@ library(jsonlite)
 library(dplyr)
 library(here)
 library(readr)
+library(hydroGOF)
 
 project_path <- here("swat", "TxtInOut")
 trial_dir <- commandArgs(trailingOnly=TRUE)[1]
@@ -14,7 +15,9 @@ data <- read_json(path=file.path(trial_dir, "parameters.json"))
 
 start_date <- data$start_date
 end_date <- data$end_date
-data <- within(data, rm(start_date, end_date))
+years_skip <- data$years_skip
+# validation_start <- data$validation_start
+data <- within(data, rm(start_date, end_date, years_skip))
 
 par_comb = c()
 for (name in names(data)) {
@@ -25,28 +28,46 @@ sim <- run_swatplus(
     project_path=project_path,
     start_date = start_date,
     end_date = end_date,
-    years_skip = 0,
+    years_skip = years_skip,
     run_path=trial_dir,
-    keep_folder=TRUE,
     parameter = par_comb,
     output = define_output(
             file = 'channel_sd_day',
             variable = 'flo_out',
             unit = 1)
         )
+# print("sim!!!")
+# print(sim)
 
-obs<-read_csv(here("swat","obs_data.csv"))
-obs <- obs %>% filter(between(date, as.Date(start_date), as.Date(end_date)))
+obs <- read_csv(here("swat","obs_data.csv"))
+obs <- obs %>% filter(between(date, min(sim$simulation$flo_out$date), as.Date(end_date)))
 
 flow_obs <- obs %>% filter(variable == "flow_cms") %>% within(rm(variable))
 flow_sim <- sim$simulation$flo_out %>% filter(date %in% flow_obs$date)
 
-if (!is.null(flow_sim)) {
+# print("flow sim and flow obs!!!")
+# print(flow_sim)
+# print(flow_obs)
+
+# # RMSE
+# if (!is.null(flow_sim)) {
+#     jsn <- toJSON(list(
+#         flo_out=list(
+#             y_pred=flow_sim$run_1,
+#             y_true=flow_obs$value
+#         )
+#     ), pretty = TRUE)
+#     write(jsn, file.path(trial_dir, "output.json"))
+# } else {
+#     write(toJSON(list(trial_status=unbox("FAILED"))), file.path(trial_dir, "output.json"))
+# }
+
+# NSE (Nash–Sutcliffe model efficiency coefficient)
+nse <- NSE(sim=flow_sim$run_1, obs=flow_obs$value)
+if (!is.null(nse)) {
+
     jsn <- toJSON(list(
-        flo_out=list(
-            y_pred=flow_sim$run_1,
-            y_true=flow_obs$value
-        )
+        flo_out_NSE=nse
     ), pretty = TRUE)
     write(jsn, file.path(trial_dir, "output.json"))
 } else {
