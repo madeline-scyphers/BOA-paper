ec34d9a57a3620cc9e461754f933fd66979b4c49
Madeline Scyphers <madelinescyphers@gmail.com>
2023-10-27 11:48:42 -0400

    slurm scripts

--- slurm_restart_boa.sh
+++ slurm_restart_boa.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=boa_run
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1 --ntasks-per-node=28
 #SBATCH --output=%j.log
 #SBATCH --account=PAS0409
 
 set -x
 
-config_path=$1
+scheduler_path=$1
 
 echo 'starting optimization'
 
-python -m boa --config-path $config_path
+python -m boa -sp $scheduler_path
 
 echo 'completed optimization'`
