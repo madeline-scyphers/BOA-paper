8df3d44337c5670833ba7b5996c7271eae8eedcb
Madeline Scyphers <madelinescyphers@gmail.com>
2024-07-13 14:34:30 -0400

    hartmann example bug fixes

--- r_hartmann/run_model_w_l2norm.R
+++ r_hartmann/run_model_w_l2norm.R
@@ -5,4 +5,4 @@ source("./hartmann6.R")
 trial_dir <- commandArgs(trailingOnly=TRUE)[1]
 data <- read_json(path=file.path(trial_dir, "parameters.json"))
 res <- hartmann6(unlist(data))
-write(toJSON(list(Hartmann6=res)), file.path(trial_dir, "output.json"))
+write(toJSON(list(Hartmann6=res, l2norm=sum(unlist(data)**2)**(1/2))), file.path(trial_dir, "output.json"))
