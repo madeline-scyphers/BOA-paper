8f9f3c3907986fed25547e948577fffb27d2e91f
Madeline Scyphers <madelinescyphers@gmail.com>
2024-07-18 19:36:33 -0400

    Update examples with comments and more shown options

--- r_hartmann/run_model.R
+++ r_hartmann/run_model.R
@@ -4,5 +4,6 @@ source("./hartmann6.R")
 
 trial_dir <- commandArgs(trailingOnly=TRUE)[1]
 data <- read_json(path=file.path(trial_dir, "parameters.json"))
-res <- hartmann6(unlist(data))
+X <- c(data$x1, data$x2, data$x3, data$x4, data$x5, data$x6)
+res <- hartmann6(X)
 write(toJSON(list(Hartmann6=res)), file.path(trial_dir, "output.json"))
